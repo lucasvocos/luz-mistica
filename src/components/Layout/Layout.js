c1a6fc709e16c66c23bc5d2bb183dbf927ff29f2
Lucas Vocos <lucasvocos@gmail.com>
2020-04-11 11:31:22 -0400

    initial commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../Header";
+import Footer from "../Footer";
+import "./Layout.scss";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
